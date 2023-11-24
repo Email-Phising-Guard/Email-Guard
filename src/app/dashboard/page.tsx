6d520707818576474e0a1cc1d5204ee79e7d3743
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-11-24 13:32:47 -0800

    Finish dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const DashboardLinks = [
+  { href: "/dashboard", label: "Explore Topics" },
+  { href: "/quiz", label: "Start Quiz" },
+];
+
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <main className="mx-auto max-w-md">
+      <h1 className="mt-20 text-center">Dashboard</h1>
+      <div className="mx-auto mt-32 flex w-full max-w-xs flex-col gap-5">
+        {DashboardLinks.map(({ href, label }) => (
+          <Link key={href} href={href}>
+            <Button className="w-full py-10 text-2xl font-light">
+              {label}
+            </Button>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }
