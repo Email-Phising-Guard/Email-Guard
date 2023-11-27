d4eec0a53f74d5f9fd86729065dd973bafd87bae
scottchen98 <scott.chen1117@gmail.com>
2023-11-27 14:18:28 -0800

    create tips and display them in accordions

--- src/app/dashboard/topic/identifying-email-phishing/page.tsx
+++ src/app/dashboard/topic/identifying-email-phishing/page.tsx
@@ -1,12 +1,48 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import Navbar from "@/components/navbar";
+
+const tips = [
+  {
+    header: "Check the Sender's Email Address",
+    content:
+      "Examine the sender's email address carefully. Phishing emails often use addresses that resemble, but do not exactly match, the official domain.",
+  },
+  {
+    header: "Look for Spelling and Grammar Errors",
+    content:
+      "Phishing emails may contain spelling and grammar mistakes. Legitimate organizations usually maintain a high level of professionalism in their communications.",
+  },
+  {
+    header: "Verify Email Greetings",
+    content:
+      'Generic greetings like "Dear Customer" instead of your actual name can be a red flag. Legitimate organizations often personalize their emails.',
+  },
+];
 
 export default function TopicTwoPage() {
   return (
     <>
       <Navbar type="back" />
       <MaxWidthWrapper>
-        <h1 className="mt-10">Identifying Email Phishing</h1>
+        <h1 className="my-10">Identifying Email Phishing</h1>
+        <h2 className="mb-5 text-3xl">Tips</h2>
+        <div className="space-y-5">
+          {tips.map((tip, index) => (
+            <Accordion type="single" collapsible key={index}>
+              <AccordionItem value={`item-${index}`}>
+                <AccordionTrigger>{tip.header}</AccordionTrigger>
+                <AccordionContent>{tip.content}</AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))}
+        </div>
       </MaxWidthWrapper>
     </>
   );
