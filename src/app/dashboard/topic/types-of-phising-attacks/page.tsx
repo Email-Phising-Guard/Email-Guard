3de889c38e48f33b30cd9035a3eed3122c24d338
AliceEHZz <meiyellmya@gmail.com>
2023-11-27 13:57:14 -0800

    nav to types of phising attacks

--- src/app/dashboard/topic/types-of-phising-attacks/page.tsx
+++ src/app/dashboard/topic/types-of-phising-attacks/page.tsx
@@ -5,7 +5,18 @@ export default function TopicOnePage() {
   return (
     <>
       <Navbar type="back" />
-      <MaxWidthWrapper>TopicOnePage</MaxWidthWrapper>
+      <MaxWidthWrapper>
+        <h1 className="py-5">Email Phising</h1>
+        <section className="py-5">
+          <h2 className="mb-3">What is it?</h2>
+          <p>
+            Email phishing is a deceitful tactic involving fake emails that
+            trick people into sharing personal information or taking harmful
+            actions. Stay cautious and verify unexpected emails to avoid falling
+            for these scams.
+          </p>
+        </section>
+      </MaxWidthWrapper>
     </>
   );
 }
